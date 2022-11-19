54971107f879d027c72c228c30fc5416bffade40
Mmmccc <ch.minji.k@gmail.com>
2022-11-19 17:22:31 -0500

    Buttons

--- manager.js
+++ manager.js
@@ -2,7 +2,7 @@ const tabs = await chrome.tabs.query({});
 const selected = [];
 const groups = [];
 
-tabs.sort((a,b) => a.index-b.index);
+tabs.sort((a, b) => a.index - b.index);
 
 const template = document.getElementById("popup");
 const elements = [];
@@ -20,44 +20,44 @@ for (const tab of tabs) {
     //selecting pages
     element.querySelector("a").addEventListener("click", async () => {
 
-    if (selected.includes(tab)) {
-        if (tab.groupId > -1) {
-            const group = tab.groupId;
-            for(const i of tabs) {
-                if (i.groupId == group){
-                    const index = selected.indexOf(i);
-                    selected.splice(index,1);
-                    const tabIndex = tabs.indexOf(i);
-                    elements[tabIndex].classList.remove("selected");
+        if (selected.includes(tab)) {
+            if (tab.groupId > -1) {
+                const group = tab.groupId;
+                for (const i of tabs) {
+                    if (i.groupId == group) {
+                        const index = selected.indexOf(i);
+                        selected.splice(index, 1);
+                        const tabIndex = tabs.indexOf(i);
+                        elements[tabIndex].classList.remove("selected");
+                    }
                 }
+            }
+            else {
+                const index = selected.indexOf(tab);
+                if (index > -1) {
+                    selected.splice(index, 1);
+                    element.classList.remove("selected");
+                }
+
             }
         }
         else {
-            const index = selected.indexOf(tab);
-            if (index > -1){
-                selected.splice(index,1);
-                element.classList.remove("selected");
-        }
-        
-      }
-    }
-    else {
-        if (tab.groupId > -1) {
-            const group = tab.groupId;
-            for(const i of tabs) {
-                if (i.groupId == group){
-                    selected.push(i);
-                    const tabIndex = tabs.indexOf(i);
-                    elements[tabIndex].classList.add("selected");
+            if (tab.groupId > -1) {
+                const group = tab.groupId;
+                for (const i of tabs) {
+                    if (i.groupId == group) {
+                        selected.push(i);
+                        const tabIndex = tabs.indexOf(i);
+                        elements[tabIndex].classList.add("selected");
+                    }
                 }
             }
+            else {
+                selected.push(tab);
+                element.classList.add("selected");
+            }
+
         }
-        else{
-            selected.push(tab);
-            element.classList.add("selected"); 
-        }
-        
-    }
     })
 
     /////// changing tab focus
@@ -65,7 +65,7 @@ for (const tab of tabs) {
         await chrome.tabs.update(tab.id, { active: true });
         await chrome.windows.update(tab.windowId, { focused: true });
     })
-    
+
     /////// closing tabs
     element.querySelector("a").addEventListener("contextmenu", async (event) => {
         event.preventDefault();
@@ -78,35 +78,34 @@ for (const tab of tabs) {
     /////// create group
     const button = document.querySelector(".group");
     button.addEventListener("click", async () => {
-    const tabIds = selected.map(({ id }) => id);
-    const group = await chrome.tabs.group({ tabIds });
+        const tabIds = selected.map(({ id }) => id);
+        const group = await chrome.tabs.group({ tabIds });
 
-    for (const tab of tabs) {
-     const index = selected.indexOf(tab);
-     selected[index].classList.add('grouped');
-  }
+        for (const tab of tabs) {
+            const index = selected.indexOf(tab);
+            selected[index].classList.add('grouped');
+        }
+    });
 
-  
-});
+    // /////// find keyword in selected tabs
+    // const findButton = document.querySelector(".find");
+    // findButton.addEventListener("click", async () => {
+    //     const tabIds = selected.map(({ id }) => id);
+    //     // const group = await chrome.tabs.group({ tabIds });
+
+    //     for (const tab of tabs) {
+    //         const index = selected.indexOf(tab);
+    //         selected[index].classList.add('grouped');
+    //     }
+    // });
+
 }
 
 reloadList(document.querySelector("ul"));
 
-const button = document.querySelector(".group");
-button.addEventListener("click", async () => {
-  const tabIds = selected.map(({ id }) => id);
-  const group = await chrome.tabs.group({ tabIds });
-
-  /*for (const tab of selected) {
-    const index = selected.indexOf(tab);
-    selected[index].classList.add('grouped');
-  }*/
-  
-});
-
 function reloadList(element) {
     while (element.firstChild) {
-      element.removeChild(element.firstChild);
+        element.removeChild(element.firstChild);
     }
     element.append(...elements);
-  }
+}
